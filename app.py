57e4d23cf271bd85a663272a3408539c423013cf
aiarabelo <49424364+aiarabelo@users.noreply.github.com>
2019-05-02 17:25:38 -0700

    Added exercise 3.02

--- app.py
+++ app.py
@@ -9,6 +9,14 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/exercise3-02')
+def exercise3_02():
+	return render_template('exercise3a.html')
+
+@app.route('/exercise3-03')
+def exercise_03():
+	return render_template('exercise3b.html')
+
 @app.errorhandler(404)
 def page_not_found(e):
     # note that we set the 404 status explicitly
